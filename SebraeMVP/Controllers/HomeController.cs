7b00158df0fbb041722a24819e2cd91f518ce50d
Sissa92 <45231031+Sissa92@users.noreply.github.com>
2020-05-04 23:26:55 -0300

    Initial commit

---
+++ SebraeMVP/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SebraeMVP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult PerfilScreem()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroScreem()
+        {
+            return View();
+        }
+
+        public ActionResult ServicoScreem()
+        {
+            return View();
+        }
+
+        public ActionResult FinalizadoScreem()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroSenhaScreem()
+        {
+            return View();
+        }
+
+        public ActionResult LoginScreem()
+        {
+            return View();
+
+        }
+
+        public ActionResult HomeScreem()
+        {
+            return View();
+
+        }
+
+    }
+
+}
